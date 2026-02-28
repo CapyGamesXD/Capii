fd7040825dcd5e999603a53c606e7c34352e98ae
CapyGamesXD <luke.robert.steenkamp@gmail.com>
2026-02-28 20:15:21 +0000

    Bug fix

--- src/lib/versions.js
+++ src/lib/versions.js
@@ -1,4 +1,4 @@
 // @ts-ignore
-export let version = '1.1.0';
+export let version = '1.1.1';
 export let logs =
 	'New update! Added a capybara page, alongside the selection page! Hope you enjoy! :D';
