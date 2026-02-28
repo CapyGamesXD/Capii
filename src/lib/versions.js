550ff8924582fd80151f5ab76c3a63d4940fa24d
CapyGamesXD <luke.robert.steenkamp@gmail.com>
2026-02-28 20:54:30 +0000

    Stopwatch page fix

--- src/lib/versions.js
+++ src/lib/versions.js
@@ -1,4 +1,4 @@
 // @ts-ignore
 export let version = '1.1.5';
 export let logs =
-	'Added a stopwatch app, alongside various bug fixes, including stopping the horizontal overlap.';
+	'Added a stopwatch app, alongside various bug fixes, including stopping the horizontal overlap and scaling issues on the stopwatch page.';
