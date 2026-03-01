9519a9812f122359c62469a9631bb34a3b5d2417
CapyGamesXD <luke.robert.steenkamp@gmail.com>
2026-03-01 09:10:43 +0000

    Refined update process to avoid flashing refreshes.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
+	import { register } from 'module';
 
 	import '../styling.css';
 	let { children } = $props();
@@ -8,7 +9,10 @@
 	onMount(() => {
 		navigator.serviceWorker.register('/sw.js').then((res) =>
 			res.addEventListener('updatefound', () => {
-				window.location.reload();
+				const newestWorker = res.installing;
+				if (newestWorker?.state === 'activated') {
+					window.location.reload();
+				}
 			})
 		);
 	});
