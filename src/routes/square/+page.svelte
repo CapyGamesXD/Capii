afc7ac1a5c7175cea241062f9eaffdc924e9ae09
CapyGamesXD <luke.robert.steenkamp@gmail.com>
2026-03-01 09:40:45 +0000

    Improved the square page.

--- src/routes/square/+page.svelte
+++ src/routes/square/+page.svelte
@@ -8,6 +8,7 @@
 <div class="centerdiv">
 	<a class="back" href="selector">Back</a>
 	<div class="square">
-		This square should be perfectly even on both axes. Measure to test for stretching on the screen.
+		<div class="smallSquare"></div>
 	</div>
+	This square should be perfectly even on both axes. Measure to test for stretching on the screen.
 </div>
